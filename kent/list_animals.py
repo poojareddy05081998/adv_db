98ef18f4fd23a3b2c41f2a78aa63a89fe4bc88d3
poojareddy05081998 <58837202+poojareddy05081998@users.noreply.github.com>
2020-10-04 23:01:18 -0400

    simple web application

--- kent/list_animals.py
+++ kent/list_animals.py
@@ -2,6 +2,6 @@ import sqlite3
 
 connection = sqlite3.connect("animals.db")
 cursor = connection.cursor()
-cursor.execute("select * from pets where kind='Dog'")
+cursor.execute("select * from pets where kind='cat'")
 result = cursor.fetchall()
 print(result)
